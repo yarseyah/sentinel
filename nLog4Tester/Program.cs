2bc1692bc3c13f89e4ad0df0122e4f28cdfa0e9e
yarseyah <ray.hayes@gmail.com>
2015-08-19 16:15:37 +0100

    Added simple tester using nlog4.  Basically identical to the other versions.

---
+++ nLog4Tester/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace nLog4Tester
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    using NLog;
+
+    public static class Program
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        private static readonly Random Random = new Random();
+
+        private static readonly List<string> Reasons = new List<string>
+                                                           {
+                                                               "Starting system",
+                                                               "Closing system",
+                                                               "Data exchange started",
+                                                               "Unknown issue encountered"
+                                                           };
+
+        private static readonly List<string> Sources = new List<string>
+                                                           {
+                                                               "Foo",
+                                                               "Bar",
+                                                               "LongSystemName",
+                                                               "Kernel32"
+                                                           };
+
+        static void Main(string[] args)
+        {
+            const int SmallestSleep = 1000;
+            const int BiggestSleep = 2000;
+
+            for (var i=0;;i++)
+            {
+                // Randomly generate a message:
+                var text = RandomMessage(i++);
+
+                // Randomly assign a message
+                LogMessage(text);
+
+                Thread.Sleep(Random.Next(SmallestSleep, BiggestSleep));
+            }  
+        }
+
+
+        private static void LogMessage(string text)
+        {
+            var randomType = Random.Next(0, 6);
+
+            switch (randomType)
+            {
+                case 0:
+                    Log.Error(text);
+                    break;
+                case 1:
+                    Log.Fatal(text);
+                    break;
+                case 2:
+                    Log.Info(text);
+                    break;
+                case 3:
+                    Log.Warn(text);
+                    break;
+                case 4:
+                    Log.Trace(text);
+                    break;
+                default:
+                    Log.Debug(text);
+                    break;
+            }
+        }
+
+        private static string RandomMessage(int i)
+        {
+#if !TESTING_MESSAGE_THROUGHPUT
+            var randomNumber = Random.Next(0, 5);
+#else
+            var randomNumber = Int32.MaxValue;
+#endif
+            switch (randomNumber)
+            {
+                case 0:
+                    return string.Format("Message {0}", i);
+                case 1:
+                    return string.Format("Src:'{0}', Msg:'{1} - {2}'", RandomSrc(), RandomReason(), i);
+                case 2:
+                    return string.Format("[{0}] {1} - {2}", RandomSrc(), RandomReason(), i);
+                case 3:
+                    return string.Format("[SimulationTime] {0} ({1})", RandomReason(), i);
+                case 4:
+                    return "UTF-8 test code: \u2019 \u263b \u2660 \u2663 \u2665 \u2666";
+                default:
+                    return i.ToString();
+            }
+        }
+
+        private static string RandomReason()
+        {
+            return Reasons[Random.Next(Reasons.Count)];
+        }
+
+        private static string RandomSrc()
+        {
+            return Sources[Random.Next(Sources.Count)];
+        }
+    }
+}
