d13df102bff5a4d5ed869963802da5332f460673
Ray Hayes <ray.hayes@gmail.com>
2018-02-09 16:01:07 +0000

    Version number incremented

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,8 +2,8 @@ using System;
 using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyVersion("0.13.0")]
-[assembly: AssemblyFileVersion("0.13.0")]
+[assembly: AssemblyVersion("0.13.1")]
+[assembly: AssemblyFileVersion("0.13.1")]
 
 // Ensure all assemblies have a neutral language defined.
 [assembly: NeutralResourcesLanguage("en")]
@@ -11,4 +11,4 @@ using System.Resources;
 // Addres CA1014 issues
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyCopyright("Copyright © Ray Hayes 2009-2017")]
+[assembly: AssemblyCopyright("Copyright © Ray Hayes 2009-2018")]
