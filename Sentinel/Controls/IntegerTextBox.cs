feb3dde5895a33f608b64b153e4b6ffc46719807
Ray Hayes <ray.hayes@gmail.com>
2020-04-04 13:34:29 +0100

    Limiting input in 'MaximumMessageCount' to integers
    added new, very simple, IntegerTextBox control

---
+++ Sentinel/Controls/IntegerTextBox.cs
@@ -0,0 +1,16 @@
+﻿namespace Sentinel.Controls
+{
+    using System.Linq;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    public class IntegerTextBox : TextBox
+    {
+        protected override void OnPreviewTextInput(TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(char.IsDigit);
+
+            base.OnPreviewTextInput(e);
+        }
+    }
+}
