cf1def96bd59143606666ea7865e979ad60e19b0
yarseyah <ray.hayes@gmail.com>
2020-04-05 15:51:46 +0100

    Code to reinflate a FileMonitorProvider was not implemented (#19)
    Added implementation, but this code is in need of a major refactor as it is high maintenance for new providers.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,9 +1,9 @@
-using System;
+﻿using System;
 using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyVersion("0.14.0")]
-[assembly: AssemblyFileVersion("0.14.0")]
+[assembly: AssemblyVersion("0.14.1.0")]
+[assembly: AssemblyFileVersion("0.14.1.0")]
 
 // Ensure all assemblies have a neutral language defined.
 [assembly: NeutralResourcesLanguage("en")]
@@ -12,3 +12,5 @@ using System.Resources;
 [assembly: CLSCompliant(true)]
 
 [assembly: AssemblyCopyright("Copyright � Ray Hayes 2009-2020")]
+
+[assembly: AssemblyInformationalVersion("0.14.0+Branch.master.Sha.03a8c8a60675462fbe27f9e075ce02ce1b1071b1")]
