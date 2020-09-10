7a9206720afdfd8ce6bda0002362bb104c160989
A9G-Data-Droid <akauffman@cmm-measure.com>
2020-09-10 09:53:37 -0700

    Remove Splat ReSharper Optimize references

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -13,4 +13,4 @@ using System.Resources;
 
 [assembly: AssemblyCopyright("Copyright © Ray Hayes 2009-2020")]
 
-[assembly: AssemblyInformationalVersion("0.14.2-UpdateAllFrameWorks.1+5.Branch.UpdateAllFrameWorks.Sha.0e581bfe8bf9267a2b566209bc4753df369916b3")]
+[assembly: AssemblyInformationalVersion("0.14.2-UpdateAllFrameWorks.1+7.Branch.UpdateAllFrameWorks.Sha.8a94b1206e6fb581a78b616fb62cef16447a9b07")]
