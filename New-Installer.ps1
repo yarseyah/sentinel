4e4004110b37a773f0fa4659cba4073e9fe5b0ce
Ray Hayes <ray.hayes@gmail.com>
2020-09-19 18:40:50 +0100

    Optimising parameters for New-Installer.ps1

--- New-Installer.ps1
+++ New-Installer.ps1
@@ -6,7 +6,7 @@
 .EXAMPLE
     New-Installer
 .EXAMPLE
-    New-Installer --NoCompile --NoVersionUpdate
+    New-Installer --NoCompile --Version 1.2.3
 .INPUTS
     None
 .OUTPUTS
@@ -28,16 +28,16 @@ param (
 
     [Alias("v")]
     [Switch]
-    $NoVersionUpdate = $false
+    [string] $Version = $null
 )
 
 function Get-MsBuildCommand() {
 
-    Write-Verbose "Get-MsBuildCommand()"
+    Write-Output "Get-MsBuildCommand()"
 
     $findCommand = Get-Command msbuild -ErrorAction SilentlyContinue
     if ( $null -eq $findCommand ) {
-        Write-Verbose "Get-MsBuildCommand() - returning 'msbuild' due to Get-Command finding something"
+        Write-Output "Get-MsBuildCommand() - returning 'msbuild' due to Get-Command finding something"
         return "msbuild"
     }
 
@@ -46,9 +46,9 @@ function Get-MsBuildCommand() {
     #   and can be found at the following location:
     #   %ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
 
-    Write-Verbose "Get-MsBuildCommand() - using vswhere to find"
+    Write-Output "Get-MsBuildCommand() - using vswhere to find"
     $vsWhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-    Write-Verbose "Get-MsBuildCommand() - looking for vswhere at $vsWhere"
+    Write-Output "Get-MsBuildCommand() - looking for vswhere at $vsWhere"
 
     if ( $null -eq (Get-Command $vsWhere -ErrorAction SilentlyContinue) ) {
         Write-Error "Unable to find 'vswhere'"
@@ -57,7 +57,7 @@ function Get-MsBuildCommand() {
         $msbuild = & $vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
 
         if ( Test-Path $msbuild ) {
-            Write-Verbose "Get-MsBuildCommand() - found at $msbuild"
+            Write-Output "Get-MsBuildCommand() - found at $msbuild"
             return $msbuild
         }
         else {
@@ -65,7 +65,7 @@ function Get-MsBuildCommand() {
         }
     }
 
-    Write-Verbose "Get-MsBuildCommand() - giving up and returning $null"
+    Write-Output "Get-MsBuildCommand() - giving up and returning $null"
     return $null;
 }
 
@@ -159,7 +159,7 @@ $nuspecFile = "Sentinel.nuspec"
 
 $buildConfiguration = "Release"
 
-if ( -not $NoBuild ) {
+if ( $null -ne $Version -and -not $NoBuild ) {
     # Build the code in release mode
     & $msbuild /t:Clean /p:Configuration=$buildConfiguration $solutionFile
     & $msbuild /t:Build /p:Configuration=$buildConfiguration $solutionFile
@@ -171,41 +171,43 @@ if ( -not $NoBuild ) {
 }
 
 $basePath = "$applicationName\Bin\$buildConfiguration"
-Write-Host "Base Path = $basePath"
+Write-Output "Base Path = $basePath"
 
 $application = "$basePath\$applicationName.exe"
-Write-Host "Application = $application"
+Write-Output "Application = $application"
 
-$version = (get-item $application).VersionInfo.FileVersion
-Write-Debug (get-item $application).VersionInfo
+if ( $null -ne $version ) {
+    $version = (get-item $application).VersionInfo.FileVersion
+    Write-Output (get-item $application).VersionInfo
+}
 
 # See whether there is a symver truncation of version
-Write-Verbose "Determining version"
+Write-Output "Determining version"
 $symver = $version -replace '(\d+)\.(\d+)\.(\d+)\.(\d+)', '$1.$2.$3'
 if ( $symver -ne $version ) {
-    Write-Information "SymVer version being used: $($version) -> $($symver)"
+    Write-Output "SymVer version being used: $($version) -> $($symver)"
     $version = $symver
 }
 
-Write-Verbose "Creating Nuget package to contain installation content"
+Write-Output "Creating Nuget package to contain installation content"
 Write-Output "Packaging up $application [$version]"
 & $nuget pack -NoPackageAnalysis $nuspecFile -Version $version -BasePath $basePath
 $package = "$productName.$version.nupkg"
 
 if ( Test-Path $package ) {
-    Write-Verbose "Processing nuget package: $package"
+    Write-Output "Processing nuget package: $package"
     Write-Output "Packed into $package"
     $releasesLocation = "Releases"
 
     # Squirrel doesn't wait, so cheat by pipelining it with | Write-Output
     # See https://github.com/Squirrel/Squirrel.Windows/issues/489
-    Write-Verbose "Involking Squirrel"
+    Write-Output "Involking Squirrel"
     & $squirrel --releasify $package --no-msi --releaseDir=$releasesLocation | Write-Output
 
     if ( $LASTEXITCODE -eq 0) {
         $srcFile = Join-Path $releasesLocation "setup.exe"
         $destFile = Join-Path $releasesLocation "$($productName)-Setup-$version.exe"
-        Write-Verbose "Renaming output file: src: $srcFile dest: $destFile"
+        Write-Output "Renaming output file: src: $srcFile dest: $destFile"
 
         # Give the installer a sensible name
         Move-Item $srcFile $destFile -Force
