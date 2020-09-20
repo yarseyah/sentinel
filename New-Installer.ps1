1dd868bec606cbba48569133d51b3cd815767656
Ray Hayes <ray.hayes@gmail.com>
2020-09-20 12:09:55 +0100

    Improving Squirrel build

--- New-Installer.ps1
+++ New-Installer.ps1
@@ -27,16 +27,16 @@ param (
     $NoBuild = $false,
 
     [Alias("v")]
-    [string] $Version = $null
+    [string] $Version = [string]::Empty
 )
 
 function Get-MsBuildCommand() {
 
-    Write-Output "Get-MsBuildCommand()"
+    Write-Verbose "Looking for MSBuild"
 
     $findCommand = Get-Command msbuild -ErrorAction SilentlyContinue
     if ( $null -eq $findCommand ) {
-        Write-Output "Get-MsBuildCommand() - returning 'msbuild' due to Get-Command finding something"
+        # returning 'msbuild' due to Get-Command finding something
         return "msbuild"
     }
 
@@ -45,9 +45,8 @@ function Get-MsBuildCommand() {
     #   and can be found at the following location:
     #   %ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
 
-    Write-Output "Get-MsBuildCommand() - using vswhere to find"
+    Write-Verbose "Using vswhere to find msbuild"
     $vsWhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-    Write-Output "Get-MsBuildCommand() - looking for vswhere at $vsWhere"
 
     if ( $null -eq (Get-Command $vsWhere -ErrorAction SilentlyContinue) ) {
         Write-Error "Unable to find 'vswhere'"
@@ -56,7 +55,7 @@ function Get-MsBuildCommand() {
         $msbuild = & $vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
 
         if ( Test-Path $msbuild ) {
-            Write-Output "Get-MsBuildCommand() - found at $msbuild"
+            Write-Host "Get-MsBuildCommand() - found at $msbuild"
             return $msbuild
         }
         else {
@@ -64,11 +63,12 @@ function Get-MsBuildCommand() {
         }
     }
 
-    Write-Output "Get-MsBuildCommand() - giving up and returning $null"
+    # giving up and returning $null
     return $null;
 }
 
 function Get-NugetCommand() {
+    Write-Verbose "Looking for nuget"
     $findCommand = Get-Command nuget -ErrorAction SilentlyContinue
     if ( $null -ne $findCommand ) {
         return "nuget"
@@ -83,6 +83,7 @@ function Get-NugetCommand() {
 }
 
 function Get-SquirrelCommand() {
+    Write-Verbose "Looking for Squirrel.exe"
     $findCommand = Get-Command squirrel.exe -ErrorAction SilentlyContinue
     if ( $null -ne $findCommand ) {
         return "squirrel.exe"
@@ -98,6 +99,7 @@ function Get-SquirrelCommand() {
 }
 
 function Get-GitVerionsCommand() {
+    Write-Verbose "Looking for GitVersion"
     $findCommand = Get-Command nuget -ErrorAction SilentlyContinue
     if ( $null -ne $findCommand ) {
         return "gitversion"
@@ -112,42 +114,54 @@ function Get-GitVerionsCommand() {
     }
 }
 
-$msbuild = Get-MsBuildCommand
-if ( $null -eq $msbuild ) {
-    Write-Error "Unable to locate MSBuild"
-    Exit
+if ( -not $NoBuild ) {
+    $msbuild = Get-MsBuildCommand
+    if ( $null -eq $msbuild ) {
+        Write-Error "Unable to locate MSBuild"
+        Exit 1
+    }
+
+    Write-Host "MSBuild = $msbuild"
 }
 
 $nuget = Get-NugetCommand
 if ( $null -eq $nuget ) {
     Write-Error "Unable to locate Nuget"
-    Exit
+    Exit 1
 }
 
-Write-Output "MSBuild = $msbuild"
-Write-Output "Nuget = $nuget"
+Write-Host "Nuget = $nuget"
 
-# Restore any packages
-& $nuget restore
-
-# Look for locations of things that may have required nuget restore to download
 $squirrel = Get-SquirrelCommand
+
+if ( -not $NoBuild ) {
+    # Restore any packages
+    & $nuget restore
+
+    # If clean building, Squirrel might not be found until a nuget restore is done
+    if ( $null -eq $squirrel) {
+        $squirrel = Get-SquirrelCommand
+    }
+}
+
 if ( $null -eq $squirrel ) {
     Write-Error "Unable to locate Squirrel"
-    Exit
+    Exit 1
 }
 
-Write-Output "Squirrel = $squirrel"
+Write-Host "Squirrel = $squirrel"
 
-if ( -not $NoVersionUpdate ) {
+if ( [string]::IsNullOrWhiteSpace($Version) ) {
     $gitversion = Get-GitVerionsCommand
     if ( $null -eq $gitversion ) {
         Write-Error "Unable to locate GitVersion"
-        Exit
+        Exit 1
     }
 
-    Write-Output "GitVersion = $gitversion"
+    Write-Host "GitVersion = $gitversion"
     & $gitversion /updateAssemblyInfo SharedAssemblyInfo.cs
+} else {
+    Write-Host "Using supplied version of $version"
 }
 
 # ===========================================================================
@@ -158,55 +172,55 @@ $nuspecFile = "Sentinel.nuspec"
 
 $buildConfiguration = "Release"
 
-if ( $null -ne $Version -and -not $NoBuild ) {
+if ( -not $NoBuild ) {
     # Build the code in release mode
     & $msbuild /t:Clean /p:Configuration=$buildConfiguration $solutionFile
     & $msbuild /t:Build /p:Configuration=$buildConfiguration $solutionFile
 
     if ($LASTEXITCODE -ne 0) {
         Write-Error "Failed to build!"
-        Exit
+        Exit 1
     }
 }
 
 $basePath = "$applicationName\Bin\$buildConfiguration"
-Write-Output "Base Path = $basePath"
+Write-Host "Base Path = $basePath"
 
 $application = "$basePath\$applicationName.exe"
-Write-Output "Application = $application"
+Write-Host "Application = $application"
 
-if ( $null -ne $version ) {
+if ( [string]::IsNullOrWhiteSpace($version) ) {
     $version = (get-item $application).VersionInfo.FileVersion
-    Write-Output (get-item $application).VersionInfo
+    Write-Host (get-item $application).VersionInfo
+
+    # See whether there is a symver truncation of version
+    Write-Host "Determining version"
+    $symver = $version -replace '(\d+)\.(\d+)\.(\d+)\.(\d+)', '$1.$2.$3'
+    if ( $symver -ne $version ) {
+        Write-Host "SymVer version being used: $($version) -> $($symver)"
+        $version = $symver
+    }
 }
 
-# See whether there is a symver truncation of version
-Write-Output "Determining version"
-$symver = $version -replace '(\d+)\.(\d+)\.(\d+)\.(\d+)', '$1.$2.$3'
-if ( $symver -ne $version ) {
-    Write-Output "SymVer version being used: $($version) -> $($symver)"
-    $version = $symver
-}
-
-Write-Output "Creating Nuget package to contain installation content"
-Write-Output "Packaging up $application [$version]"
+Write-Host "Creating Nuget package to contain installation content"
+Write-Host "Packaging up $application [$version]"
 & $nuget pack -NoPackageAnalysis $nuspecFile -Version $version -BasePath $basePath
 $package = "$productName.$version.nupkg"
 
 if ( Test-Path $package ) {
-    Write-Output "Processing nuget package: $package"
-    Write-Output "Packed into $package"
+    Write-Host "Processing nuget package: $package"
+    Write-Host "Packed into $package"
     $releasesLocation = "Releases"
 
-    # Squirrel doesn't wait, so cheat by pipelining it with | Write-Output
+    # Squirrel doesn't wait, so cheat by pipelining it with | Write-Host
     # See https://github.com/Squirrel/Squirrel.Windows/issues/489
-    Write-Output "Involking Squirrel"
-    & $squirrel --releasify $package --no-msi --releaseDir=$releasesLocation | Write-Output
+    Write-Host "Involking Squirrel"
+    & $squirrel --releasify $package --no-msi --releaseDir=$releasesLocation | Write-Host
 
     if ( $LASTEXITCODE -eq 0) {
         $srcFile = Join-Path $releasesLocation "setup.exe"
         $destFile = Join-Path $releasesLocation "$($productName)-Setup-$version.exe"
-        Write-Output "Renaming output file: src: $srcFile dest: $destFile"
+        Write-Host "Renaming output file: src: $srcFile dest: $destFile"
 
         # Give the installer a sensible name
         Move-Item $srcFile $destFile -Force
@@ -215,6 +229,9 @@ if ( Test-Path $package ) {
     }
 }
 else {
-    Write-Output "File $($package) not found"
+    Write-Host "File $($package) not found"
     Write-Error "Problem creating package"
 }
+
+Write-Host "`$LastExitCode = $LASTEXITCODE"
+Exit -1
